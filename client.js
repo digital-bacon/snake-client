472fb36422ff8e73af1882e80be6fe1e69c9ae38
Mark Bieganek <mark.bieganek@telus.com>
2022-11-23 13:47:45 -0700

    Send player movement input to the server

--- client.js
+++ client.js
@@ -3,7 +3,8 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
+    // host: '165.227.47.243',
+    host: 'localhost',
     port: '50541',
   }, () => console.log('Connected to server.'));
 
@@ -15,7 +16,7 @@ const connect = function () {
     conn.write('Name: XXX');
     const delay = 100;
     let currentTimer = delay;
-    setInterval(() => conn.write('Move: down'), 100);
+    // setInterval(() => conn.write('Move: down'), 100);
     // setInterval(() => conn.write('Move: right'), 35);
     // setInterval(() => conn.write('Move: up'), 100);
     // setInterval(() => conn.write('Move: left'), 30);
--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,7 +17,15 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     console.log('Returning you to your boring shell.')
     process.exit();
-  }
+  };
+  if (key === 'w')
+    connection.write('Move: up');
+  if (key === 'a')
+    connection.write('Move: left');
+  if (key === 's')
+    connection.write('Move: down');
+  if (key === 'd')
+    connection.write('Move: right');
 };
 
 module.exports = { setupInput }
