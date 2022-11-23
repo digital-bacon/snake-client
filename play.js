97bf4be1c5c965937e828db27d767d9053bb85b5
Mark Bieganek <mark.bieganek@telus.com>
2022-11-23 09:34:11 -0700

    Add net connection, handle event data from server

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '172.20.212.31',
+    port: '50541',
+  }, () => console.log('Connected to server.'));
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log('Game started!')
+  });
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
