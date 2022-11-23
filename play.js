b5f108d6e0c033f236c57bf368535b7163bfb44b
Mark Bieganek <mark.bieganek@telus.com>
2022-11-23 13:28:47 -0700

    Add a way to listen for user input

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
 const { connect } = require('./client')
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    console.log('Returning you to your boring shell.')
-    process.exit();
-  }
-};
+const { setupInput } = require('./input')
 
 console.log("Connecting ...");
 connect();
