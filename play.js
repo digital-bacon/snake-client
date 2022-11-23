215e36551839e952394c6bb4f02f2c7e99d45998
Mark Bieganek <mark.bieganek@telus.com>
2022-11-23 10:47:43 -0700

    Handle user input: exit game with CTRL+C

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const { connect } = require('./client')
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    console.log('Returning you to your boring shell.')
+    process.exit();
+  }
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
