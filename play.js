1023967b5417737449e953d319724b05690dfccc
Mark Bieganek <mark.bieganek@telus.com>
2022-11-23 09:39:26 -0700

    Move net createConnection logic to client.js

--- play.js
+++ play.js
@@ -1,25 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '172.20.212.31',
-    port: '50541',
-  }, () => console.log('Connected to server.'));
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log('Game started!')
-  });
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  });
-
-  return conn;
-};
+const connect = require('./client')
 
 console.log("Connecting ...");
 connect();
