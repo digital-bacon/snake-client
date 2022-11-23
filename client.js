5c0e24307f483853fd154ed82c5d0111aa666834
Mark Bieganek <mark.bieganek@telus.com>
2022-11-23 09:51:14 -0700

    Send player initials to the server

--- client.js
+++ client.js
@@ -11,7 +11,8 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log('Game started!')
+    console.log('Game started!');
+    conn.write('Name: MB');
   });
   conn.on('data', (data) => {
     console.log(data.toString());
